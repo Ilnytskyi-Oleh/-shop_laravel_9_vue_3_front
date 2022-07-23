404a0ad03de2a657768d73971d663ed3b6e906df
Valdogar <67922@ukr.net>
2022-07-23 16:48:22 +0300

    base

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,15 @@
-<script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
+<template>
+  <div>
+    Home View
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeView"
+}
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped>
+
+</style>
