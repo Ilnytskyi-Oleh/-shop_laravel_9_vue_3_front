f8491a58fbd18d1213193ee156b2a57fbf7502ad
Valdogar <67922@ukr.net>
2022-07-23 17:11:40 +0300

    +BAse tenplate

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
-
   <router-link :to="{name:'Home'}">Home</router-link>
   <router-link :to="{name:'About'}">About</router-link>
   <router-view></router-view>
 </template>
+
 <script>
   export default {
     name: "App"
   }
 </script>
-<style scoped></style>
+
+<style scoped>
+
+</style>
